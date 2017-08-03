2491f5438b5cae96927228c8a51314d0e781116f
fup1990 <fup1990@163.com>
2017-08-03 15:09:48 +0800

    自定义异常

---
+++ easyid-common/src/main/java/com/gome/fup/easyid/exception/NoMoreValueInRedisException.java
@@ -0,0 +1,11 @@
+package com.gome.fup.easyid.exception;
+
+/**
+ * Created by fupeng-ds on 2017/8/3.
+ */
+public class NoMoreValueInRedisException extends Exception{
+
+    public NoMoreValueInRedisException(String message) {
+        super(message);
+    }
+}
