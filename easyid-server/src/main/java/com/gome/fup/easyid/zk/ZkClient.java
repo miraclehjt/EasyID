f99b0f43fb7ffd9246b124e8f1107284d7c20b6e
fup1990 <fup1990@163.com>
2017-08-03 10:54:07 +0800

    ZooKeeper客户端类

---
+++ easyid-server/src/main/java/com/gome/fup/easyid/zk/ZkClient.java
@@ -0,0 +1,100 @@
+package com.gome.fup.easyid.zk;
+
+import com.gome.fup.easyid.util.ConversionUtil;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.io.IOException;
+
+/**
+ * ZooKeeper客户端类，在ZooKeeper上注册服务信息，并实现负载均衡
+ * Created by fupeng-ds on 2017/8/3.
+ */
+public class ZkClient {
+
+    /**
+     * ZooKeeper上的根节点
+     */
+    private final String ZK_ROOT_NODE = "/EasyID";
+
+    /**
+     * ZooKeeper客户端
+     */
+    private ZooKeeper zooKeeper;
+
+    /**
+     *ZooKeeper服务地址
+     */
+    private String address;
+
+    /**
+     * ZooKeeper会话有效时间
+     */
+    private final static int SESSIONTIMEOUT = 10000;
+
+    /**
+     * 初始化ZooKeeper客户端，并判断是否有根节点，没有则创建持久化节点
+     * @param address
+     * @throws IOException
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    public ZkClient(String address) throws IOException, KeeperException, InterruptedException {
+        this.address = address;
+        zooKeeper = new ZooKeeper(address, SESSIONTIMEOUT, null);
+        Stat stat = zooKeeper.exists(ZK_ROOT_NODE, false);
+        if (null == stat) {
+            zooKeeper.create(ZK_ROOT_NODE, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+    }
+
+    /**
+     * 初始化节点，并记录连接数为0
+     * @param node
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    public void register(String node) throws KeeperException, InterruptedException {
+        zooKeeper.create(getNodePath(node), ConversionUtil.intToByteArray(0), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    }
+
+    /**
+     * 增加节点的记录数
+     * @param node
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    public synchronized int increase(String node) throws KeeperException, InterruptedException {
+        String path = getNodePath(node);
+        int count = getCount(path);
+        zooKeeper.setData(path, ConversionUtil.intToByteArray(++count), -1);
+        return count;
+    }
+
+    /**
+     * 减少节点的记录数
+     * @param node
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    public synchronized int decrease(String node) throws KeeperException, InterruptedException {
+        String path = getNodePath(node);
+        int count = getCount(path);
+        if (count > 0) {
+            zooKeeper.setData(path, ConversionUtil.intToByteArray(--count), -1);
+        }
+        return count;
+    }
+
+    public int getCount(String path) throws KeeperException, InterruptedException {
+        byte[] data = zooKeeper.getData(path, null, null);
+        return ConversionUtil.byteArrayToInt(data);
+    }
+
+    private String getNodePath(String node) {
+        return ZK_ROOT_NODE + "/" + node;
+    }
+}
