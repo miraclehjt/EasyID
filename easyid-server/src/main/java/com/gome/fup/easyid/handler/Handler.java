6f4fac0e01ef57d6dc4aa5e761bdee2fef7bc30e
fup1990 <fup1990@163.com>
2017-08-04 14:03:13 +0800

    zk节点负载计数器

--- easyid-server/src/main/java/com/gome/fup/easyid/handler/Handler.java
+++ easyid-server/src/main/java/com/gome/fup/easyid/handler/Handler.java
@@ -3,8 +3,10 @@ package com.gome.fup.easyid.handler;
 import com.gome.fup.easyid.model.Request;
 import com.gome.fup.easyid.snowflake.Snowflake;
 import com.gome.fup.easyid.util.Constant;
+import com.gome.fup.easyid.util.IpUtil;
 import com.gome.fup.easyid.util.KryoUtil;
 import com.gome.fup.easyid.util.MessageType;
+import com.gome.fup.easyid.zk.ZkClient;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -25,9 +27,13 @@ public class Handler extends SimpleChannelInboundHandler<Request> {
 
     private Snowflake snowflake;
 
+    private ZkClient zkClient;
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Request request) throws Exception {
         if (request.getType() == MessageType.REQUEST_TYPE_CREATE) {
+            String ip = IpUtil.getLocalHost();
+            zkClient.increase(ip);
             redisTemplate.execute(new RedisCallback<Object>() {
                 public Object doInRedis(RedisConnection connection) throws DataAccessException {
                     byte[] key = KryoUtil.objToByte(Constant.REDIS_LIST_NAME);
@@ -41,12 +47,38 @@ public class Handler extends SimpleChannelInboundHandler<Request> {
                     return null;
                 }
             });
+            zkClient.decrease(ip);
             ctx.writeAndFlush("").addListener(ChannelFutureListener.CLOSE);
         }
     }
 
-    public Handler(RedisOperations<Serializable, Serializable> redisTemplate, Snowflake snowflake) {
+    public Handler(RedisOperations<Serializable, Serializable> redisTemplate, Snowflake snowflake, ZkClient zkClient) {
         this.redisTemplate = redisTemplate;
         this.snowflake = snowflake;
+        this.zkClient = zkClient;
+    }
+
+    public RedisOperations<Serializable, Serializable> getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    public void setRedisTemplate(RedisOperations<Serializable, Serializable> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Snowflake getSnowflake() {
+        return snowflake;
+    }
+
+    public void setSnowflake(Snowflake snowflake) {
+        this.snowflake = snowflake;
+    }
+
+    public ZkClient getZkClient() {
+        return zkClient;
+    }
+
+    public void setZkClient(ZkClient zkClient) {
+        this.zkClient = zkClient;
     }
 }
