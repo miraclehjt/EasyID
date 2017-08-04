fd0f3c0b7dde16f513e029c6aff33055572564be
fup1990 <fup1990@163.com>
2017-08-04 11:46:35 +0800

    添加常量类

---
+++ easyid-common/src/main/java/com/gome/fup/easyid/util/Constant.java
@@ -0,0 +1,18 @@
+package com.gome.fup.easyid.util;
+
+/**
+ * Created by fupeng-ds on 2017/8/1.
+ */
+public class Constant {
+
+    /**
+     * server port
+     */
+    public static final int EASYID_SERVER_PORT = 9131;
+
+    /**
+     * redis key
+     */
+    public static final String REDIS_LIST_NAME = "EasyID";
+
+}
