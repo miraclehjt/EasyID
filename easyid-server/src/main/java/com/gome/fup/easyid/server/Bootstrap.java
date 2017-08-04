77ec36d58325c7e408c4bc59d34ec943002a3cc5
fup1990 <fup1990@163.com>
2017-08-04 11:49:12 +0800

    引导程序

--- easyid-server/src/main/java/com/gome/fup/easyid/server/Bootstrap.java
+++ easyid-server/src/main/java/com/gome/fup/easyid/server/Bootstrap.java
@@ -1,6 +1,12 @@
 package com.gome.fup.easyid.server;
 
+import com.gome.fup.easyid.snowflake.Snowflake;
+import com.gome.fup.easyid.util.IpUtil;
+import com.gome.fup.easyid.zk.ZkClient;
+import org.apache.zookeeper.KeeperException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
 
 /**
  * EasyID服务启动入口
@@ -8,8 +14,49 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Bootstrap {
 
-    public static void main(String[] args) {
-        new ClassPathXmlApplicationContext("classpath:spring-config.xml");
+    /**
+     * 例：java -jar EasyID.jar -zk127.0.0.1:2181 -workerid10 -datacenterid11
+     * @param args
+     * @throws IOException
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
+        if (args != null || args.length > 0) {
+            for (String arg : args) {
+                analysis(context, arg);
+            }
+        }
+        ZkClient client = context.getBean(ZkClient.class);
+        client.register(IpUtil.getLocalHost());
+    }
+
+    /**
+     * 解析参数
+     * 参数说明：
+     *         -zk127.0.0.1:2181， zookeeper地址
+     *         -workerid10， snowflake的工作id，取值范围0~31
+     *         -datacenterid11， snowflake的工作中心id，取值范围0~31
+     * @param context
+     * @param arg
+     */
+    private static void analysis(ClassPathXmlApplicationContext context, String arg) {
+        if (arg.contains("-zk")) {
+            String[] split = arg.split("-zk");
+            ZkClient client = context.getBean(ZkClient.class);
+            client.setAddress(split[1]);
+        }
+        if (arg.contains("-workerid")) {
+            String[] split = arg.split("-workerid");
+            Snowflake snowflake = context.getBean(Snowflake.class);
+            snowflake.setWorkerId(Long.parseLong(split[1]));
+        }
+        if (arg.contains("-datacenterid")) {
+            String[] split = arg.split("-datacenterid");
+            Snowflake snowflake = context.getBean(Snowflake.class);
+            snowflake.setDatacenterId(Long.parseLong(split[1]));
+        }
     }
 
 }
