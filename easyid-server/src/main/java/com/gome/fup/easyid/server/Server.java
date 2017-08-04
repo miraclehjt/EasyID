3d79452b57a60bb65392dd8e1924cb1af487ffce
fup1990 <fup1990@163.com>
2017-08-04 11:49:01 +0800

    netty服务类

---
+++ easyid-server/src/main/java/com/gome/fup/easyid/server/Server.java
@@ -0,0 +1,98 @@
+package com.gome.fup.easyid.server;
+
+import com.gome.fup.easyid.handler.DecoderHandler;
+import com.gome.fup.easyid.handler.Handler;
+import com.gome.fup.easyid.model.Request;
+import com.gome.fup.easyid.snowflake.Snowflake;
+import com.gome.fup.easyid.util.Constant;
+import com.gome.fup.easyid.util.IpUtil;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.apache.log4j.Logger;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.data.redis.core.RedisOperations;
+
+import java.io.Serializable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 服务端，接收创建id的请求
+ * Created by fupeng-ds on 2017/8/2.
+ */
+public class Server implements Runnable, InitializingBean, ApplicationContextAware{
+
+    private static final Logger logger = Logger.getLogger(Server.class);
+
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    private RedisOperations<Serializable, Serializable> redisTemplate;
+
+    private Snowflake snowflake;
+
+    public void afterPropertiesSet() throws Exception {
+        executorService.submit(this);
+        logger.info("EasyID Server started!");
+    }
+
+    public void run() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel)
+                                throws Exception {
+                            socketChannel.pipeline()
+                                    .addLast(new DecoderHandler(Request.class))
+                                    .addLast(new Handler(redisTemplate, snowflake));
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture future = bootstrap.bind(IpUtil.getLocalHost(), Constant.EASYID_SERVER_PORT).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+            executorService.shutdown();
+        }
+    }
+
+    public RedisOperations<Serializable, Serializable> getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    public void setRedisTemplate(RedisOperations<Serializable, Serializable> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public Snowflake getSnowflake() {
+        return snowflake;
+    }
+
+    public void setSnowflake(Snowflake snowflake) {
+        this.snowflake = snowflake;
+    }
+
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.snowflake = context.getBean(Snowflake.class);
+        this.redisTemplate = context.getBean(RedisOperations.class);
+    }
+}
