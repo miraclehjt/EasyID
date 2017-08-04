5394a4c2624c253845f3b6c64fdee9e769561401
fup1990 <fup1990@163.com>
2017-08-04 11:48:08 +0800

    zk测试类

--- easyid-server/src/test/java/com/gome/fup/easyid/zk/ZkClientTest.java
+++ easyid-server/src/test/java/com/gome/fup/easyid/zk/ZkClientTest.java
@@ -1,7 +1,15 @@
 package com.gome.fup.easyid.zk;
 
+import com.gome.fup.easyid.snowflake.Snowflake;
+import com.gome.fup.easyid.util.KryoUtil;
 import org.apache.zookeeper.KeeperException;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.io.IOException;
 
@@ -28,5 +36,23 @@ public class ZkClientTest {
         ZkClient client = new ZkClient("127.0.0.1:2181");
         int increase = client.decrease("testRegister");
         System.out.println(increase);
+    }
+
+    @Test
+    public void testRedis() {
+        String key = "EasyID";
+        final byte[] bytes = KryoUtil.objToByte(key);
+        Snowflake snowflake = new Snowflake();
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
+        RedisTemplate redisTemplate = context.getBean(RedisTemplate.class);
+        long id = snowflake.nextId();
+        System.out.println(id);
+        final byte[] value = KryoUtil.objToByte(id);
+        redisTemplate.execute(new RedisCallback() {
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.rPush(bytes,value);
+            }
+        });
+
     }
 }
