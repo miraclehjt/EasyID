383fadd04e0d3acd9ccb7b4aa17302effbb6dbc9
fup1990 <fup1990@163.com>
2017-08-04 11:45:40 +0800

    自定义异常

--- easyid-common/src/main/java/com/gome/fup/easyid/exception/NoMoreValueInRedisException.java
+++ easyid-common/src/main/java/com/gome/fup/easyid/exception/NoMoreValueInRedisException.java
@@ -3,7 +3,7 @@ package com.gome.fup.easyid.exception;
 /**
  * Created by fupeng-ds on 2017/8/3.
  */
-public class NoMoreValueInRedisException extends Exception{
+public class NoMoreValueInRedisException extends RuntimeException{
 
     public NoMoreValueInRedisException(String message) {
         super(message);
---
+++ easyid-common/src/main/java/com/gome/fup/easyid/exception/ZooKeeperNoAddressException.java
@@ -0,0 +1,11 @@
+package com.gome.fup.easyid.exception;
+
+/**
+ * Created by fupeng-ds on 2017/8/3.
+ */
+public class ZooKeeperNoAddressException extends RuntimeException{
+
+    public ZooKeeperNoAddressException(String message) {
+        super(message);
+    }
+}
