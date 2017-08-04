8f9563c00984fdf55c413aeb3e2da237e01f2c0e
fup1990 <fup1990@163.com>
2017-08-04 11:46:16 +0800

    编码解码handler

---
+++ easyid-common/src/main/java/com/gome/fup/easyid/handler/DecoderHandler.java
@@ -0,0 +1,45 @@
+package com.gome.fup.easyid.handler;
+
+import com.gome.fup.easyid.util.KryoUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * 
+ *
+ * @author fupeng-ds
+ */
+public class DecoderHandler extends ByteToMessageDecoder{
+	
+	private Class<?> type;
+	
+	public DecoderHandler(Class<?> type) {
+		super();
+		this.type = type;
+	}
+
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf in,
+			List<Object> out) throws Exception {
+		if(in.readableBytes() < 4) {
+			return;
+		}
+		in.markReaderIndex();
+		int dataLength = in.readInt();
+		if(dataLength < 0) {
+			ctx.close();
+		}
+		if(in.readableBytes() < dataLength) {
+			in.resetReaderIndex();
+		}
+		byte[] data = new byte[dataLength];
+		in.readBytes(data);
+		Object obj = KryoUtil.byteToObj(data, type);
+		out.add(obj);
+	}
+
+}
