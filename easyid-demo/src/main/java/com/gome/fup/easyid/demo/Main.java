471f801219790ffebfda682c48766ee63c23403b
fup1990 <fup1990@163.com>
2017-08-04 14:49:23 +0800

    关闭spring容器

--- easyid-demo/src/main/java/com/gome/fup/easyid/demo/Main.java
+++ easyid-demo/src/main/java/com/gome/fup/easyid/demo/Main.java
@@ -11,11 +11,10 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
         EasyID easyID = context.getBean(EasyID.class);
-        System.out.println(System.currentTimeMillis());
         for (int i = 0; i < 3000; i++) {
             long id = easyID.nextId();
             System.out.println("EasyID nextId " + i + " : " + id);
         }
-        System.out.println(System.currentTimeMillis());
+        context.close();
     }
 }
