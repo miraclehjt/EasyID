a49fa56cd05bb7058d9acc7f06bd1f568acffd46
fup1990 <fup1990@163.com>
2017-08-04 14:01:46 +0800

    main方法

--- easyid-demo/src/main/java/com/gome/fup/easyid/demo/Main.java
+++ easyid-demo/src/main/java/com/gome/fup/easyid/demo/Main.java
@@ -11,7 +11,11 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
         EasyID easyID = context.getBean(EasyID.class);
-        long id = easyID.nextId();
-        System.out.println("EasyID nextId : " + id);
+        System.out.println(System.currentTimeMillis());
+        for (int i = 0; i < 3000; i++) {
+            long id = easyID.nextId();
+            System.out.println("EasyID nextId " + i + " : " + id);
+        }
+        System.out.println(System.currentTimeMillis());
     }
 }
