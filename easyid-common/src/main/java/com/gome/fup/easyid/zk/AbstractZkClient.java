cfa55ba07d2b975e415526df075322c2c4fe5e13
fup1990 <fup1990@163.com>
2017-08-07 13:51:57 +0800

    根据节点数，计算redis队列长度系数

--- easyid-common/src/main/java/com/gome/fup/easyid/zk/AbstractZkClient.java
+++ easyid-common/src/main/java/com/gome/fup/easyid/zk/AbstractZkClient.java
@@ -1,16 +1,21 @@
 package com.gome.fup.easyid.zk;
 
+import com.gome.fup.easyid.util.Cache;
+import com.gome.fup.easyid.util.Constant;
 import com.gome.fup.easyid.util.ConversionUtil;
+import com.gome.fup.easyid.util.KryoUtil;
 import org.apache.log4j.Logger;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  *ZkClient抽象类
@@ -63,6 +68,38 @@ public abstract class AbstractZkClient {
                 logger.info("zookeeper type : " + event.getType() + ", and state" + event.getState());
             }
         });
+    }
+
+    /**
+     * 获取/EasyID根节点下子节点的数量
+     * @return
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    private int getRootChildren() throws KeeperException, InterruptedException {
+        List<String> nodes = zooKeeper.getChildren(ZK_ROOT_NODE, null, null);
+        return nodes.size();
+    }
+
+    /**
+     * redis队列数量
+     * 动态获取
+     * @return
+     */
+    public int getRedisListSize() {
+        int size;
+        if (Cache.hasKey(Constant.REDIS_LIST_SIZE)) {
+            size = (Integer) Cache.get(Constant.REDIS_LIST_SIZE);
+        } else {
+            try {
+                size = this.getRootChildren();
+                Cache.set(Constant.REDIS_LIST_SIZE, size);
+            } catch (Exception e) {
+                logger.error(e);
+                size = 1;
+            }
+        }
+        return size;
     }
     
 }
