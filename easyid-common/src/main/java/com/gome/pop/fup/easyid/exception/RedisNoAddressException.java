73b6bf9374ad7f1c707c562c8f03914fd59b8cd8
fup1990 <fup1990@163.com>
2017-08-18 14:00:13 +0800

    RedisNoAddress异常

---
+++ easyid-common/src/main/java/com/gome/pop/fup/easyid/exception/RedisNoAddressException.java
@@ -0,0 +1,11 @@
+package com.gome.pop.fup.easyid.exception;
+
+/**
+ * Created by fupeng-ds on 2017/8/3.
+ */
+public class RedisNoAddressException extends RuntimeException{
+
+    public RedisNoAddressException(String message) {
+        super(message);
+    }
+}
